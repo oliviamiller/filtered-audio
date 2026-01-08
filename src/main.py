581d43f0e0ed7fb0cf4f841a2d41d007be76e737
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2026-01-08 14:56:01 -0500

    Revert "Revert "wake word filter""
    This reverts commit 225b092e9f108b31151295259e77e6db9625cdd6.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import asyncio
 from viam.module.module import Module
 try:
-    from models.trigger import Trigger
+    from models.wake_word_filter import WakeWordFilter
 except ModuleNotFoundError:
     # when running as local module with run.sh
-    from .models.trigger import Trigger
+    from .models.wake_word_filter import WakeWordFilter
 
 
 if __name__ == '__main__':
